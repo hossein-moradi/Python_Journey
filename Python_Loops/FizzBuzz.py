c92c98c0c5621069021d70f6247d98137a6362a5
Hossein_Moradi <150090250+hossein-moradi@users.noreply.github.com>
2026-06-13 18:55:09 +0330

    new proj

--- Python_Loops/FizzBuzz.py
+++ Python_Loops/FizzBuzz.py
@@ -5,7 +5,6 @@
 # If the number is divisible by 5, then instead of printing the number it should print "Buzz".`
 # And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
 
-
 for i in range(1,101):
 
     if i % 3 == 0 and i % 5 == 0:
