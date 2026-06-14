c38f6bef79d8a13c83744c0e3a717b24772de1e8
Hossein_Moradi <150090250+hossein-moradi@users.noreply.github.com>
2026-06-14 20:23:13 +0330

    we got the password

--- Python_Loops/NewPassword.py
+++ Python_Loops/NewPassword.py
@@ -13,14 +13,25 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7',
            '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*',
            '+']
-
+password = []
 print("Welcome to the PyPassword Generator!")
 number_of_letters = int(input("How many letters would you like in your password?\n"))
 number_of_symbols = int(input(f"How many symbols would you like?\n"))
 number_of_numbers = int(input(f"How many numbers would you like?\n"))
 
 for x in range(number_of_letters):
-    print(random.choices(letters))
+    password.append(random.choice(letters))
+
+for y in range(number_of_symbols):
+    password.append(random.choices(symbols))
+
+for z in range(number_of_numbers):
+    password.append(random.choices(numbers))
+print(password)
+
+
+
+
 
 
 
