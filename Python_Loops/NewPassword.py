fe36a837863e5017ac91b768f361f04c9486edf9
Hossein_Moradi <150090250+hossein-moradi@users.noreply.github.com>
2026-06-14 19:51:07 +0330

    we got the for loop

--- Python_Loops/NewPassword.py
+++ Python_Loops/NewPassword.py
@@ -1,15 +1,28 @@
-# So this exercise is about a password creator
+# So this exercise is about a password creating
+
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+           'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+           'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+           'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
+           'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+           'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+           'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7',
+           '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*',
+           '+']
 
 print("Welcome to the PyPassword Generator!")
 number_of_letters = int(input("How many letters would you like in your password?\n"))
 number_of_symbols = int(input(f"How many symbols would you like?\n"))
 number_of_numbers = int(input(f"How many numbers would you like?\n"))
 
-x = random.randint(letters[number_of_letters])
-print(x)
-print(numbers[number_of_symbols])
-print(numbers[number_of_numbers])
+for x in range(number_of_letters):
+    print(random.choices(letters))
+
+
+
+# print(numbers[number_of_symbols])
+# print(numbers[number_of_numbers])
