7d4e2616b4bad82fe7c9025e7474d2261bd39183
Hossein_Moradi <150090250+hossein-moradi@users.noreply.github.com>
2026-06-16 16:08:24 +0330

    Hurdle4

--- Python_Loops/Reeborg Robot.py
+++ Python_Loops/Reeborg Robot.py
@@ -60,3 +60,32 @@ while not at_goal():
     else :
         jump()
 # Robot will go around itself forever
+
+
+# Soooooooo eventually i did it myself ( i didn`t know there was a wall_on_right() function)
+# so here it is :
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    else:
+        jump()
+# With this code you can pass the Hurdle4 challenge, i will put down the sites link below:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
