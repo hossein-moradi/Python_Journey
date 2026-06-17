7f6065fa39dde53e9d5463f533b3c4c635c72399
Hossein_Moradi <150090250+hossein-moradi@users.noreply.github.com>
2026-06-17 20:05:21 +0330

    maze

--- Python_Loops/Reeborg Robot.py
+++ Python_Loops/Reeborg Robot.py
@@ -3,6 +3,7 @@
 # So i am at the Hurdle4 i did eventually get the robot to the goal but problem is he does not stop
 # The main problem is i made too much functions.
 # Here check out my code:
+"""
 def turn_right():
     turn_left()
     turn_left()
@@ -40,11 +41,12 @@ while not at_goal():
         move()
     else:
         jump()
-
+"""
 
 # Now i am going to delete and retry everything from scratch
 
 # Check this out i got my first infinit loop XD
+"""
 def turn_right():
     turn_left()
     turn_left()
@@ -60,10 +62,12 @@ while not at_goal():
     else :
         jump()
 # Robot will go around itself forever
+"""
 
 
 # Soooooooo eventually i did it myself ( i didn`t know there was a wall_on_right() function)
 # so here it is :
+"""
 def turn_right():
     turn_left()
     turn_left()
@@ -87,5 +91,36 @@ while not at_goal():
         move()
     else:
         jump()
+"""
 # With this code you can pass the Hurdle4 challenge, i will put down the sites link below:
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+
+
+# Maze challenge:
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+"""
+For this challenge you just have to know 1 rule and that`s the right-hand roul:
+1. If there is no wall on the right, turn right and move.
+2. Otherwise, if there is no wall in front, move forward.
+3. Otherwise, turn left (because you're blocked on both the right and the front).
+"""
+
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def check():
+    if not wall_on_right():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+        
+while not at_goal():
+    check()
+"""
